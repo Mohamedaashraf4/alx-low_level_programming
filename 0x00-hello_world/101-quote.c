70518a1793d591e3c3271d1ad76967a0acf18588
ashraaaf <mohammed.aashraf4@gmail.com>
2023-08-11 16:14:33 -0700

    ashraf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Description: A C program that prints
+ * Description: print a qouta using write function
+ *	ssize_t write(int fd. const void *buf, size_t count);
  *
  * Return: 1 (error).
 */
+
 
 int main(void)
 {	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
