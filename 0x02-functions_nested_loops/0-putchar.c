c44f980f52fe51111ccd77d8d55f8ce3824196ad
ashraaaf <mohammed.aashraf4@gmail.com>
2023-08-22 09:44:44 -0700

    ashraf

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,16 @@
 #include "main.h"
-	/**
-	 * main - print _putchar
-	 * is written by mbah
-	 * Return: 0
-	 */
+
+/**
+ * main: - print _putchar
+ *
+ * Description: prints putchar using putchar prototype
+ *
+ * Return: 0
+*/
 
 
 	int main(void)
-	{
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -18,4 +21,4 @@
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-	}
+}
