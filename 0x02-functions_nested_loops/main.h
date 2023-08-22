d966bccf6e28a18dd8f4fc35936f3912b2396496
ashraaaf <mohammed.aashraf4@gmail.com>
2023-08-22 13:07:11 -0700

    ashraf

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,14 +3,4 @@
 #include <stdio.h>
 
 	int _putchar(char c);
-		_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
 #endif
