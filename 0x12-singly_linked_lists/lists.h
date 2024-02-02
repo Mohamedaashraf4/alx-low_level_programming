1908aa3cb10097733cba0fde19bd03373b5bd60a
ashraaaf <mohammed.aashraf4@gmail.com>
2024-02-02 13:41:28 -0800

    ashraf

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -18,3 +19,12 @@ typedef struct list_s
     unsigned int len;
     struct list_s *next;
 } list_t;
+
+int _putchar(char c);
+int_strlen(char *s);
+size_t print_list(const list_t *h);
+size_t list_len(const _list_t *h);
+void free_list(list_t *head);
+void _constructor(void) _attribute_ ((constructor));
+
+#endif
